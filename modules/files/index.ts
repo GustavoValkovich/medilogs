e9dba2e08aa9fd1e646739472f7879f1ef0d24ff
Gustavo <gustavo.valkovich>
2025-08-08 13:09:29 -0300

    controlador de subudia de archivo

--- modules/files/index.ts
+++
@@ -1,4 +0,0 @@
-export { FilesController } from './files.controller';
-export { filesRouter } from './files.routes';
-export { ExcelController } from './excel.controller';
-export { excelRouter } from './excel.routes';
