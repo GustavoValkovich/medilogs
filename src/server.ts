6c5f36fe8bfa8a860d70ecba4685248454105446
Gustavo <gustavo.valkovich>
2025-10-08 16:21:03 -0300

    Test en mac y actualizacion de paquete deprecado

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Medilogs API is running on port ${PORT}`);
+  console.log(`Medilogs API is running on port ${PORT}`);
 });
